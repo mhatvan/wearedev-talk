cd5486852acfb44e68b03a50b9cf231bd864ecb0
mhatvan <markus_hatvan@aon.at>
2021-03-24 21:12:27 +0100

    - add sveltekit:prefetch to links in index.svelte - update yarn.lock to latest deps

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,38 +4,40 @@
 
 <ol>
   <li>
-    <a href="/svelte-components-are-easy-to-understand"
+    <a href="/svelte-components-are-easy-to-understand" sveltekit:prefetch
       >Svelte Components Are Easy to Understand
     </a>
   </li>
   <li style="display:none;">
-    <a href="/built-in-accessibility-and-unused-css-checks"
+    <a href="/built-in-accessibility-and-unused-css-checks" sveltekit:prefetch
       >Built-in Accessibility and unused CSS checks</a
     >
   </li>
   <li>
-    <a href="/reactivity-with-labeled-statements"
+    <a href="/reactivity-with-labeled-statements" sveltekit:prefetch
       >Reactivity With Labeled Statements</a
     >
   </li>
   <li style="display:none;">
-    <a href="/shorthand-attributes-for-passing-props"
+    <a href="/shorthand-attributes-for-passing-props" sveltekit:prefetch
       >Shorthand attributes for passing props</a
     >
   </li>
   <li>
-    <a href="/easy-global-state-management-out-of-the-box"
+    <a href="/easy-global-state-management-out-of-the-box" sveltekit:prefetch
       >Easy Global State Management Out of the Box</a
     >
   </li>
   <li>
-    <a href="/await-blocks">Await blocks</a>
+    <a href="/await-blocks" sveltekit:prefetch>Await blocks</a>
   </li>
   <li>
-    <a href="/svelte-motion-spring">Svelte Motion Spring</a>
+    <a href="/svelte-motion-spring" sveltekit:prefetch>Svelte Motion Spring</a>
   </li>
   <li>
-    <a href="/svelte-transition-fade">Svelte Transition Fade</a>
+    <a href="/svelte-transition-fade" sveltekit:prefetch
+      >Svelte Transition Fade</a
+    >
   </li>
 </ol>
 
